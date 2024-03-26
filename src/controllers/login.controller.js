18ba5bfea0a9ac936366e63edbb71da724371e3d
Robert Bills <55222340+CygnusXLR@users.noreply.github.com>
2024-03-26 00:14:57 -0700

    Add Folders
    - Added Image folder
    - Added CSS file
    - Added HTML

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,17 @@
+/**
+ * @license Apache-2.0
+ * @copyright codingforbills 2024
+ */
+
+'use strict';
+
+/**
+ * node modules
+ */
+
+const login = (req, res) => {
+    res.render('./pages/login');
+    
+}
+
+module.exports = {login  }
---
+++ src/routes/login.route.js
@@ -0,0 +1,22 @@
+/**
+ * @license Apache-2.0
+ * @copyright codingforbills 2024
+ */
+
+'use strict';
+
+/**
+ * node modules
+ */
+
+const router = require('express').Router();
+
+/**
+ * custom modules
+ */
+
+const { login } =require('../controllers/login.controller');
+
+router.get(`/`, login);
+
+module.exports = router;
